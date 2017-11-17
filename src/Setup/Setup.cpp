6b0fc23a1dec4e6e48c70d3050ddf1496ba4dfc4
FFYzz <jasonontheway8840@gmail.com>
2017-11-17 20:12:16 +0800

    read and write File succeed

--- src/Setup/Setup.cpp
+++ src/Setup/Setup.cpp
@@ -1,14 +1,35 @@
 #include "/usr/local/include/pbc/pbc.h"
 #include "/usr/local/include/pbc/pbc_test.h"
+#include "/usr/include/json/json.h"
+
+#include <string>
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
 
 const int N = 21;
 const int M = 10;
 
+
 /**
-compile program:  gcc -o test Setup.c -L. -lpbc -lgmp
+compile program:  g++ -o test Setup.cpp -L. -lpbc -lgmp -ljson
+
 execute Executable file:  ./test < ../../data/param/a.param
 **/
+
+
+
+unsigned char* transfer(element_t t){
+
+  int leng = element_length_in_bytes(t);
+  unsigned char *p = new unsigned char[leng + 1];
+  int writeLength = element_to_bytes_compressed(p,t);
+  return p;
+}
 
 int main(int argc, char **argv) {
 
@@ -45,43 +66,57 @@ int main(int argc, char **argv) {
   element_pow_zn(g1, g, y);
   element_pow_zn(v, g, z);
 
-  //create file PP to keep the variables 
-  if(freopen("../../data/setup_data/PP","w",stdout)==NULL){
-    fprintf(stderr, "error1\n");
-  }
 
-  printf("{\n");
-  element_printf("\"g1\":\"%B\",\n",g1);
-  element_printf("\"g2\":\"%B\",\n",g2);
+  //根节点  
+  Json::Value root;  
+  Json::Value root1;  
 
+  //根节点属性  
+  Json::StyledWriter sw;  
+  Json::StyledWriter sw1;  
+
+  //输出到文件  
+  ofstream os;
+  ofstream os1;  
+
+  os.open("../../data/setup_data/PP");  
+
+
+
+  root["g1"] = Json::Value((char*)transfer(g1));  
+  root["g2"] = Json::Value((char*)transfer(g2));  
   //generate elements from G
   for(int i = 0; i <= N - 1; ++i){
-  	element_init_G1(arrN[i],pairing);
-  	element_random(arrN[i]);
-    element_printf("\"t-%d\":\"%B\",\n",i+1,arrN[i]);
+    element_init_G1(arrN[i],pairing);
+    element_random(arrN[i]);
+    string index = "";
+    stringstream st;
+    st << (i+1);
+    st >> index;
+    index ="t-" + index;
+    root[index] = Json::Value((char*)transfer(arrN[i]));  
   }
 
 
-
-  element_printf("\"v'\":\"%B\",\n",v);
+  root["v"] = Json::Value((char*)transfer(v));  
   //generate elements from Zr
   for(int i = 0; i < M; ++i){
     element_init_G1(arrM[i],pairing);
-  	element_random(temp);
-  	element_pow_zn(arrM[i],g,temp);
-    element_printf("\"v-%d\":\"%B\"",i+1,arrM[i]);
-    if(i != 9){
-      printf(",\n");
-    }
-    else{
-      printf("\n");
-    }
+    element_random(temp);
+    element_pow_zn(arrM[i],g,temp);
+    string index = "";
+    stringstream st;
+    st << (i+1);
+    st >> index;
+    index ="v-" + index;
+    root[index] = Json::Value((char*)transfer(arrM[i]));  
   }
-  //end writing data to file PP 
-  printf("}\n");
-  fclose(stdout);
 
-  //create file MK to keep the variables 
+  os << sw.write(root);  
+  os.close();  
+
+
+ //create file MK to keep the variables 
   if(freopen("../../data/setup_data/MK","w",stdout)==NULL){
     fprintf(stderr, "error2\n");
   }
@@ -92,13 +127,12 @@ int main(int argc, char **argv) {
 
   fclose(stdout);
 
+  os1.open("../../data/config/config");  
+  root1["N"] = Json::Value(N);  
+  root1["M"] = Json::Value(M);  
+  os1 << sw.write(root1);  
+  os1.close();  
 
-  if(freopen("../../data/config/config","w",stdout)==NULL){
-    fprintf(stderr, "error3\n");
-  }
-
-  printf("{\n\"N\":\"%d\",\n\"M\":\"%d\"\n}",N,M);
-  fclose(stdout);
 
   //memory management
   element_clear(v);
